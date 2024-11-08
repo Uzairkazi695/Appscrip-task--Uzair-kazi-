061b9fd6533ae94ce4b8c5c480d8b68596b24812
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-11-08 17:36:05 +0530

    small error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Header from "../components/Header";
 import HeroSection from "@/components/HeroSection";
-import ProductCard from "@/components/ProductCard";
 import ProductContainer from "@/components/ProductContainer";
 import Footer from "@/components/Footer";
 export default function Home() {
